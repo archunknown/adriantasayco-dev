9a28d7d6abd81ea9fba4ab7568c50fe7509356de
archunknown <adriantasayco99@gmail.com>
2025-12-24 19:25:38 -0500

    chore: configuración inicial y archivos SEO (robots, sitemap)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "qmdobfdsuytakfvvarcz.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        // TODO: Remove after loading production data
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
